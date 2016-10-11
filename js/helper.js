011fd9ee7b30edfdca4922ad73a284219643438a
Lee, Seunghun <markandsh@gmail.com>
2016-10-11 19:55:07 -0400

    Working version of resume

--- js/helper.js
+++ js/helper.js
@@ -62,6 +62,11 @@ var googleMap = '<div id="map"></div>';
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
+function inName(string) {
+  string = string.trim().split(" ");
+  return string[0].slice(0,1).toUpperCase() + string[0].slice(1).toLowerCase() + " " + string[1].toUpperCase();
+}
+
 $(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
@@ -69,6 +74,7 @@ $(document).ready(function() {
     $name.html(iName);
   });
 });
+
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
@@ -87,97 +93,61 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
-
+var locations = [];
+function locationizer(obj) {
+  obj.forEach(function(each) {
+    locations.push(each.location);
+  }) 
+  return locations;
+}
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map;
+var service; 
+var infowindow;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.0119, lng: -98.4842},
+    zoom: 4    
+    // disableDefaultUI: true,
+  });
+  // infoWindows are the little helper windows that open when you click
+  // or hover over a pin on a map. They usually contain more information
+  // about a location.
+  infowindow = new google.maps.InfoWindow({
+    content: name,
+  });
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
-function initializeMap() {
+  pinPoster(locations);
 
-  var locations;
+  function createMarker(place) {
+  // The next lines save location data from the search result object to local variables
+    var lat = place.geometry.location.lat(); // latitude from the place service
+    var lon = place.geometry.location.lng(); // longitude from the place service
+    var name = place.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-  /*
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js.
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
-
-
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
-
-    // initializes an empty array
-    var locations = [];
-
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
-
-    // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
-
-    // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
-
-    return locations;
-  }
-
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
+    var placeLoc = place.geometry.location;
+    // marker is an object with additional data about hte pin for a single location
     var marker = new google.maps.Marker({
       map: map,
-      position: placeData.geometry.location,
+      position: place.geometry.location,
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infowindow.setContent(place.name);
+      infowindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -189,61 +159,201 @@ function initializeMap() {
     map.setCenter(bounds.getCenter());
   }
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
+        // console.log("yes");
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        // console.log(place);
+        createMarker(results[i]);
+      }
     }
   }
 
   /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
+  pinPoster(locations) takes in the array of locations created by locationFinder() and 
+  fires off Google place searches for each location
   */
   function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
+    // Creates a Google place search service object. PlaceService does the work of 
     // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
+    service = new google.maps.places.PlacesService(map);
 
-    // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
-      // the search request object
+    // Iterates through the array fo locations, creates a search object for each location
+    locations.forEach(function(place){
       var request = {
         query: place
       };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
+      // console.log(request);
+      // Actually searches the Google Maps API for location data and runs the callback 
+      // function with the search results after each session.
+      service.textSearch(request, callback);    
     });
+
   }
+}
 
   // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
+  // Calls the initializeMap() function when the page loads
+  window.addEventListener('load', initMap);
 
-}
+  // Vanilla JS way to listen for resizing of the window
+  // and adjust map bounds
+  window.addEventListener('resize', function(e) {
+    // Make sure the map bounds get updated on page resize
+   map.fitBounds(mapBounds);
+  });
+
+
+// function initMap() {
+//   var pyrmont
+// }
+
+/*
+Start here! initializeMap() is called when page is loaded.
+*/
+
+// function initializeMap() {
+
+//   var locations;
+
+//   var mapOptions = {
+//     disableDefaultUI: true
+//   };
+
+//   /*
+//   For the map to be displayed, the googleMap var must be
+//   appended to #mapDiv in resumeBuilder.js.
+//   */
+//   // map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8    
+//   });
+//   /*
+//   locationFinder() returns an array of every location string from the JSONs
+//   written for bio, education, and work.
+//   */
+//   function locationFinder() {
+
+//     // initializes an empty array
+//     var locations = [];
+
+//     // adds the single location property from bio to the locations array
+//     locations.push(resume_json.me.bio.contacts.location);
+
+//     // iterates through school locations and appends each location to
+//     // the locations array. Note that forEach is used for array iteration
+//     // as described in the Udacity FEND Style Guide:
+//     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+
+//     resume_json.me.education.schools.forEach(function(school){
+//       locations.push(school.location);
+//     });
+
+//     // iterates through work locations and appends each location to
+//     // the locations array. Note that forEach is used for array iteration
+//     // as described in the Udacity FEND Style Guide:
+//     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+//     resume_json.me.work.jobs.forEach(function(job){
+//       locations.push(job.location);
+//     });
+
+//     return locations;
+//   }
+
+//   /*
+//   createMapMarker(placeData) reads Google Places search results to create map pins.
+//   placeData is the object returned from search results containing information
+//   about a single location.
+//   */
+//   function createMapMarker(placeData) {
+
+//     // The next lines save location data from the search result object to local variables
+//     var lat = placeData.geometry.location.lat();  // latitude from the place service
+//     var lon = placeData.geometry.location.lng();  // longitude from the place service
+//     var name = placeData.formatted_address;   // name of the place from the place service
+//     var bounds = window.mapBounds;            // current boundaries of the map window
+
+//     // marker is an object with additional data about the pin for a single location
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: placeData.geometry.location,
+//       title: name
+//     });
+
+//     // infoWindows are the little helper windows that open when you click
+//     // or hover over a pin on a map. They usually contain more information
+//     // about a location.
+//     var infoWindow = new google.maps.InfoWindow({
+//       content: name
+//     });
+
+//     // hmmmm, I wonder what this is about...
+//     google.maps.event.addListener(marker, 'click', function() {
+//       // your code goes here!
+//     });
+
+//     // this is where the pin actually gets added to the map.
+//     // bounds.extend() takes in a map location object
+//     bounds.extend(new google.maps.LatLng(lat, lon));
+//     // fit the map to the new marker
+//     map.fitBounds(bounds);
+//     // center the map
+//     map.setCenter(bounds.getCenter());
+//   }
+
+//   /*
+//   callback(results, status) makes sure the search returned results for a location.
+//   If so, it creates a new map marker for that location.
+//   */
+//   function callback(results, status) {
+//     if (status == google.maps.places.PlacesServiceStatus.OK) {
+//       createMapMarker(results[0]);
+//     }
+//   }
+
+//   /*
+//   pinPoster(locations) takes in the array of locations created by locationFinder()
+//   and fires off Google place searches for each location
+//   */
+//   function pinPoster(locations) {
+
+//     // creates a Google place search service object. PlacesService does the work of
+//     // actually searching for location data.
+//     var service = new google.maps.places.PlacesService(map);
+
+//     // Iterates through the array of locations, creates a search object for each location
+//       locations.forEach(function(place){
+//       // the search request object
+//       var request = {
+//         query: place
+//       };
+
+//       // Actually searches the Google Maps API for location data and runs the callback
+//       // function with the search results after each search.
+//       service.textSearch(request, callback);
+//     });
+//   }
+
+//   // Sets the boundaries of the map based on pin locations
+//   window.mapBounds = new google.maps.LatLngBounds();
+
+//   // locations is an array of location strings returned from locationFinder()
+//   locations = locationFinder();
+
+//   // pinPoster(locations) creates pins on the map for each location in
+//   // the locations array
+//   pinPoster(locations);
+
+// }
 
 /*
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
 
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,189 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+
+// var bio = {
+// 	"name" : "James",
+// 	"age" : 32,
+// 	"role" : "Web Developer",
+// 	"contacts": {
+// 	  "mobile": "650333333",
+// 	  "email": "lee@ddd.com",
+// 	},
+// 	"welcomeMessage": "lorem ipsum",
+// 	"skills" : ["awkwardness", "programming"],
+// 	"bioPic": "images/fry.jpg",
+// };
+
+// bar.city = "Mountain View";
+// bar.email = "markandsh@gmail.com"
+
+// var resume_json = $.getJSON("js/resume.json");
+// var resume_json = (function() {
+//   var json = null;
+//   $.ajax({
+//     'async': false,
+//     'dataType': "json"
+//     'success': function (data) {
+//       json = data;
+//     }
+//   });
+//   return json;
+// })();
+// var resume_json = require("js/resume.json");
+
+var resume_json =
+{
+  "me": {
+      "bio": {
+	      "name": "Seunghun Lee",
+	      "role": "Front End Developer",
+		  "bioPic": "images/fry.jpg",
+		  "welcomeMessage": "lorem ipsum",
+	      "contacts": [
+	        {
+	          "mobile": "+1 6475709704",
+	          "email": "markandsh@gmail.com",
+	          "github": "https://github.com/Seunghunsh",
+	          "twitter": "https://twitter.com/lifeartlee",
+	          "location": "Toronto, ON, Canada"
+	        }
+	      ],
+	      "welcomeMessage": "Front end web developer who likes working in teams with great life&communication skills.",
+	      "skills": [
+	        "React.js",
+	        "Redux",
+	        "Node.js",
+	        "Javascript",
+	        "HTML",
+	        "CSS",
+	        "Front end",
+	        "Backend",
+	        "Django",
+	        "Python",
+	        "Java",
+	        "C++"
+	      ],
+	      "biopic": "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/12821536_1167450359962479_903788728259410674_n.jpg?oh=5ca327d04bc9f330f01fa32febc91ac4&oe=589D7C87",
+	      "display": "function taking no parameters"
+    },
+    "education": {
+      "schools": [
+        {
+          "name": "San Francisco State University",
+          "location": "San Francisco, CA, USA",
+          "degree": "Computer Science B.S",
+          "dates": "July, 2016"
+        }
+      ],
+      "display": "function taking no parameters"
+    },
+    "work": {
+      "jobs": [
+        {
+          "employer": "Lokafy",
+          "title": "Full Stack Developer intern",
+          "location": "Toronto, Canada",
+          "dates": "2016.08-2016.10",
+          "description": "improved social login, booking functions, look of the website."
+        },
+        {
+          "employer": "Google",
+          "title": "Web Developer intern",
+          "location": "Toronto, Canada",
+          "dates": "2016.10-2016.12",
+          "description": "improved Google Search Engine algorithm."
+        }
+      ],
+      "display": "function taking no parameters"
+    },
+    "projects": {
+      "projects": [
+        {
+          "title": "Restaurant Search app",
+          "dates": "2016-06",
+          "description": "Using several restaurant API services and sorting them to get personalized search resulsts for users. ",
+          "images": ["www.surfsf.us", "www.Twitter.com", "www.facebook.com"]
+        },
+        {
+          "title": "Twitter live stream",
+          "dates": "2016-09",
+          "description": "Using React.js, Node.js to display Twitter live stream on the website.",
+          "images": ["www.surfsf.us", "www.Twitter.com", "www.facebook.com"]
+        },
+        {
+          "title": "Movie filter",
+          "dates": "2016-08",
+          "description": "Using several movie info API services and sorting them to get personalized serach results.",
+          "images": ["www.surfsf.us", "www.Twitter.com", "www.facebook.com"]
+        },
+        {
+          "title": "E-commerce",
+          "dates": "2016-07",
+          "description": "Built E-commerce website from the scratch using python, django framework.",
+          "images": ["www.surfsf.us", "www.Twitter.com", "www.facebook.com"]
+        }
+      ],
+      "display": "function taking no parameters"
+    }
+  }
+}
+
+function displayWork() {
+	var formattedName = HTMLheaderName.replace("%data%","Seunghun Lee");
+	var formattedRole = HTMLheaderRole.replace("%data%","Front end developer");
+	var bioPic = HTMLbioPic.replace("%data%", resume_json.me.bio.bioPic);
+	var welcomeMessage = HTMLwelcomeMsg.replace("%data%", resume_json.me.bio.welcomeMessage);
+
+	$("#header").append(formattedName, formattedRole, bioPic, welcomeMessage);
+
+
+	if(resume_json.me.bio.skills[0].length > 0) {
+	  $("#header").append(HTMLskillsStart);
+	  $("#skills").append(HTMLskills.replace("%data%", resume_json.me.bio.skills));
+	};
+
+	resume_json.me.work.jobs.forEach(function(job) {
+	  $("#workExperience").append(HTMLworkStart);
+	  $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", job.employer) + HTMLworkTitle.replace("%data%", job.title));
+	  $(".work-entry:last").append(HTMLworkDates.replace("%data%", job.dates));  // $("work-entry:last")
+	  $(".work-entry:last").append(HTMLworkDescription.replace("%data%", job.description));  // $("work-entry:last")
+	});  
+}
+
+displayWork();
+
+projects.display = function() {
+  resume_json.me.projects.projects.forEach(function(project) {
+    $("#projects").append(HTMLprojectStart);
+    $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", project.title));
+    $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", project.description));
+    if (project.images.length > 0){
+      project.images.forEach(function(image){
+	    $(".project-entry:last").append(HTMLprojectImage.replace("%data%", image));
+      })
+    }
+  })
+}
+projects.display();
+
+$("#main").append(internationalizeButton);
+
+locationizer(resume_json.me.bio.contacts);
+locationizer(resume_json.me.education.schools);
+locationizer(resume_json.me.work.jobs);
+
+$("#mapDiv").append(googleMap);
+ // projects.display = function() {
+ //   for (project in projects.projects) {
+ //     $("#projects").append(HTMLprojectStart);
+
+ //     var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+ //   }
+ // }
+
+ // var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+ // $(".work-entry:last").append(formattedLocation);
+
